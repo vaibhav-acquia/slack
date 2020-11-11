a8b07d58fa68beca3e8b87ac7fdc44272c6e8544
tr <tr@202830.no-reply.drupal.org>
2020-11-11 16:35:42 +0600

    Issue #3179952 by TR: Services should generally be defined by an interface

---
+++ src/SlackInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\slack;
+
+/**
+ * Interface for the Slack service.
+ */
+interface SlackInterface {
+
+  /**
+   * Sends a message to a Slack channel.
+   *
+   * @param string $message
+   *   The message sent to the channel.
+   * @param string $channel
+   *   The channel in the Slack service to send messages.
+   * @param string $username
+   *   The bot name displayed in the channel.
+   *
+   * @return bool|object
+   *   Slack response.
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function sendMessage($message, $channel = '', $username = '');
+
+}
