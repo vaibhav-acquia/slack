da8227a1d0a220703ef0e75165d033aca932cd67
Stephen Mustgrave <38930-smustgrave@users.noreply.drupalcode.org>
2023-01-30 15:32:24 +0000

    Issue #3337322: Fix regressions in SlackRulesSendMessage

--- modules/slack_rules/src/Plugin/RulesAction/SlackRulesSendMessage.php
+++ modules/slack_rules/src/Plugin/RulesAction/SlackRulesSendMessage.php
@@ -14,7 +14,7 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
  *   id = "rules_slack_send_message",
  *   label = @Translation("Send message to Slack"),
  *   category = @Translation("Slack"),
- *   context = {
+ *   context_definitions = {
  *     "message" = @ContextDefinition("string",
  *       label = @Translation("Message"),
  *       description = @Translation("Specify the message, which should be sent to Slack."),
@@ -37,12 +37,14 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 class SlackRulesSendMessage extends RulesActionBase implements ContainerFactoryPluginInterface {
 
   /**
+   * Slack service.
+   *
    * @var \Drupal\slack\Service\SlackSendRequest
    */
   protected $slackService;
 
   /**
-   * Constructs a SlackSendMessage object.
+   * Constructs a SlackRulesSendMessage object.
    *
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
@@ -50,12 +52,12 @@ class SlackRulesSendMessage extends RulesActionBase implements ContainerFactoryP
    *   The plugin ID for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
-   * @param \Drupal\slack\Service\SlackSendRequest
+   * @param \Drupal\slack\Service\SlackSendRequest $slack_service
    *   The Slack manager service.
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, SlackSendRequest $slack_service) {
-    $this->slackService = $slack_service;
     parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->slackService = $slack_service;
   }
 
   /**
@@ -73,12 +75,12 @@ class SlackRulesSendMessage extends RulesActionBase implements ContainerFactoryP
   /**
    * Send message to slack.
    *
-   * @param string $username
-   *    The slack username.
    * @param string $message
-   *    The message to be sended.
+   *   The message to be sent.
    * @param string $channel
-   *    The slack channel.
+   *   The slack channel.
+   * @param string $username
+   *   The slack username.
    */
   protected function doExecute($message, $channel = '', $username = '') {
     $this->slackService->sendMessage($message, $channel, $username);
